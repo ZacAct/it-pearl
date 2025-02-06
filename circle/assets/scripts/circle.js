2ca03781cf4e92e386ab832e9189b3f404608078
ZacAct <158017641+ZacAct@users.noreply.github.com>
2025-02-06 20:41:38 +0000

    added new file

--- circle/assets/scripts/circle.js
+++ circle/assets/scripts/circle.js
@@ -4,31 +4,47 @@ $( "#CircleForm" ).validate({
 });
 
 function circleCalculations() {
-if ($("#CircleForm").valid()){
-let radius;
-let circumference;
-let radiusfp;
-let diameter;
+    if ($("#CircleForm").valid()) {
 
-radius = document.getElementById("radius").value;
-radiusfp = parseFloat(radius);
+        let radius;
+        let circumference;
+        let radiusfp;
+        let diameter;
+        let area;
 
-diameter = caculateDiameter(radiusfp);
-document.getElementById("diameter").innerHTML = diameter;
+        radius = document.getElementById("radius").value;
+        radiusfp = parseFloat(radius);
 
-circumference = caculateCircumference(radiusfp);
-document.getElementById("circumference").innerHTML = circumference;
-let x = 1;
+        diameter = calculateDiameter(radiusfp);
+        document.getElementById("diameter").innerHTML = diameter;
+
+        circumference = calculateCircumference(radiusfp);
+        document.getElementById("circumference").innerHTML = circumference;
+        
+
+        area = calculateArea(radiusfp);
+        document.getElementById("area").innerHTML = area;
+
+}
 }
 
-
-}
-
-
-function caculateDiamerter(r) {
+function calculateDiameter(r) {
     return 2 * r;
+
 }
 
-function caculateCircumference(r) {
+function calculateCircumference(r) {
     return 2 * Math.PI * r;
+    
+}
+
+function calculateArea(r) {
+    return Math.PI * r * r;
+}
+
+function ClearForm() {
+    document.getElementById("CircleForm").reset();
+    document.getElementById("diameter").innerHTML = "";
+    document.getElementById("circumference").innerHTML = "";
+    document.getElementById("area").innerHTML = "";
 }
